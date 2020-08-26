bcfac446dc82d6ea3c272cc87f1c1050dc1af68d
millermax <romel_23_3@hotmail.com>
2020-08-25 19:17:10 -0500

    podemos recuperar desde la base de datos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
+  //final dbref = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: Center(
-        child: Text('home page'),         
+        child: FlatButton(
+          onPressed: (){
+            _getdata();
+            },
+          child: Text('OBTENER DE FIREBASE')
+          ) ,       
       ),
     );
   }
+
+  _getdata() async{
+    await FirebaseFirestore.instance.collection('veterinarias').get().then((data){
+      print('estos son los datos $data');
+      data.docs.forEach((document){
+        print(document.data());
+
+
+      });
+
+    });
+    
+   /*  await dbref.collection('TUT').getDocuments().then((data){
+      data.documents.forEach((doc) {
+        print(doc.data);
+      });
+    });
+  } */
+
+  }
+
+
 }
